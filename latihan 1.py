f1c4ef545bad835ebe817ce82b09fd8aaff2ce0b
ZaneviantiSugirlawati <56709070+ZaneviantiSugirlawati@users.noreply.github.com>
2019-10-26 12:31:52 +0700

    Add files via upload

---
+++ latihan 1.py
@@ -0,0 +1,9 @@
+namalengkap = "Zanevianti Sugirlawati"
+namapanggilan = "zane"
+npm = "311910493"
+tempatlahir = "Bandung"
+umur = "19"
+telf = "089505372106"
+alamat = "Cikarang timur"
+
+print("assalamualaikum, \n let me introduce my self, my name is",namalengkap,", but you can call me",namapanggilan,", my npm is",npm,", i was born in",tempatlahir,"and i am",umur,"years old. i am very glad if you want to invite my house in",alamat,", so don't forget to call me before with the number",telf,"\n thank you" )
