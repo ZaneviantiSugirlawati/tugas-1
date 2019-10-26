bb3c5b58f19927b1b796b292a73cf300ab75a57e
ZaneviantiSugirlawati <56709070+ZaneviantiSugirlawati@users.noreply.github.com>
2019-10-26 12:50:02 +0700

    Add files via upload

---
+++ lab 2.py
@@ -0,0 +1,11 @@
+a=input("masukkan nilai a:")
+b=input("masukkan nilai b:")
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {1}&{0}=%d")
+
+#konversi nlai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d")
+print("hasil pembagian {1}/{0}=%d")
